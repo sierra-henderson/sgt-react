bdfc7a519b1cfef7f4ad5cb74c30082ee4323761
Sierra Henderson <henderson.sierra@yahoo.com>
2020-06-06 11:53:22 -0700

    Added change handlers

--- client/components/gradeForm.jsx
+++ client/components/gradeForm.jsx
@@ -8,6 +8,27 @@ export default class GradeForm extends React.Component {
       course: '',
       grade: ''
     };
+    this.handleNewName = this.handleNewName.bind(this);
+    this.handleNewCourse = this.handleNewCourse.bind(this);
+    this.handleNewGrade = this.handleNewGrade.bind(this);
+  }
+
+  handleNewName() {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleNewCourse() {
+    this.setState({
+      course: event.target.value
+    });
+  }
+
+  handleNewGrade() {
+    this.setState({
+      grade: event.target.value
+    });
   }
 
   render() {
@@ -17,19 +38,19 @@ export default class GradeForm extends React.Component {
           <div className="input-group-prepend">
             <div className="input-group-text">@</div>
           </div>
-          <input type="text" className="form-control" id="name" placeholder="Name" />
+          <input type="text" className="form-control" id="name" placeholder="Name" value={this.state.name} onChange={this.handleNewName}/>
         </div>
         <div className="input-group">
           <div className="input-group-prepend">
             <div className="input-group-text">@</div>
           </div>
-          <input type="text" className="form-control" id="course" placeholder="Course" />
+          <input type="text" className="form-control" id="course" placeholder="Course" value={this.state.course} onChange={this.handleNewCourse}/>
         </div>
         <div className="input-group">
           <div className="input-group-prepend">
             <div className="input-group-text">@</div>
           </div>
-          <input type="text" className="form-control" id="grade" placeholder="Grade" />
+          <input type="text" className="form-control" id="grade" placeholder="Grade" value={this.state.grade} onChange={this.handleNewGrade}/>
         </div>
         <button type="submit" className="btn btn-primary">Add</button>
         <button type="reset" className="btn btn-outline-secondary">Cancel</button>
