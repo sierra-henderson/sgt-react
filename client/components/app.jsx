6860b01308fb0eca7ed41c15524a24c889d96f71
Sierra Henderson <henderson.sierra@yahoo.com>
2020-06-05 19:39:52 -0700

    Components in separate files

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,47 +1,8 @@
 /* eslint-disable no-console, no-unused-vars */
 
 import React from 'react';
-
-class Header extends React.Component {
-  render() {
-    return <h1>Student Grade Table</h1>;
-  }
-}
-
-class Grade extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.grade.name}</td>
-        <td>{this.props.grade.course}</td>
-        <td>{this.props.grade.grade}</td>
-      </tr>
-    );
-  }
-}
-
-class GradeTable extends React.Component {
-  render() {
-    return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Course</th>
-            <th scope="col">Grade</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            this.props.grades.map(grade => {
-              return <Grade key={grade.id} grade={grade}/>;
-            })
-          }
-        </tbody>
-      </table>
-    );
-  }
-}
+import Header from './header';
+import GradeTable from './gradeTable';
 
 class App extends React.Component {
   constructor(props) {
